d38e38ea80bd0d2c0c00aa013da11c9bb0e5b1d1
IlMelons <melonss2002@gmail.com>
2025-05-18 11:49:22 +0200

    Fix Config

--- config/elevators.lua
+++ config/elevators.lua
@@ -1,43 +1,41 @@
 return {
-    Elevators = {
-        ["fib"] = {
-            label = "FIB Headquarter",
-            debug = false,
-            plans = {
-                [1] = {
-                    title = "Ground Floor",
-                    description = "Entrance and Desk",
-                    zones = {
-                        [1] = {coords = vec4(136.0807, -761.7469, 46.00, 160.00), size = vec3(2.0, 2.0, 2.5)}
-                    },
-                    locked = false,
-                    item = false,
-                    jobs = false,
+    ["fib"] = {
+        label = "FIB Headquarter",
+        debug = false,
+        floors = {
+            [1] = {
+                title = "Ground Floor",
+                description = "Entrance and Desk",
+                zones = {
+                    [1] = {coords = vec4(136.0807, -761.7469, 46.00, 160.00), size = vec3(2.0, 2.0, 2.5)}
                 },
-                [2] = {
-                    title = "Floor 47",
-                    description = "Legal Affairs",
-                    zones = {
-                        [1] = {coords = vec4(136.0939, -761.8068, 234.50, 160.00), size = vec3(2.0, 2.0, 2.5)}
-                    },
-                    locked = false,
-                    item = false,
-                    jobs = false,
-                },
-                [3] = {
-                    title = "Floor 49",
-                    description = "Communications",
-                    zones = {
-                        [1] = {coords = vec4(136.0755, -761.7881, 242.50, 160.00), size = vec3(2.0, 2.0, 2.5)}
-                    },
-                    locked = false,
-                    item = false,
-                    jobs = false,
-                }
+                locked = false,
+                item = false,
+                jobs = false,
             },
+            [2] = {
+                title = "Floor 47",
+                description = "Legal Affairs",
+                zones = {
+                    [1] = {coords = vec4(136.0939, -761.8068, 234.50, 160.00), size = vec3(2.0, 2.0, 2.5)}
+                },
+                locked = false,
+                item = false,
+                jobs = false,
+            },
+            [3] = {
+                title = "Floor 49",
+                description = "Communications",
+                zones = {
+                    [1] = {coords = vec4(136.0755, -761.7881, 242.50, 160.00), size = vec3(2.0, 2.0, 2.5)}
+                },
+                locked = false,
+                item = false,
+                jobs = false,
+            }
         },
-        ---@description Paste the presets here 🔽 [https://github.com/IlMelons/Melons-Presets/tree/main/elevators]
-
-        ---@description Paste the presets here 🔼 [https://github.com/IlMelons/Melons-Presets/tree/main/elevators]
     },
+    ---@description Paste the presets here 🔽 [https://github.com/Monarch-Development/Monarch-Presets/tree/main/elevators]
+
+    ---@description Paste the presets here 🔼 [https://github.com/Monarch-Development/Monarch-Presets/tree/main/elevators]
 }
